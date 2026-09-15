d979f8ced1c77c82021a0643706e0973e509431f
MaSidrenko <nexon863@gmail.com>
2026-09-15 20:22:53 +0300

    git[init]: initial commit2

---
+++ backend/dnd-backend/src/main/java/com/dndcrowler/dnd_backend/DndBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dndcrowler.dnd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndBackendApplication.class, args);
+	}
+
+}
---
+++ backend/dnd-backend/src/test/java/com/dndcrowler/dnd_backend/DndBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dndcrowler.dnd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
